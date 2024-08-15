3305920f289f7a0d4927f9a965721c60c6bbd94b
parksj003 <40786853+parksj003@users.noreply.github.com>
2024-08-16 08:51:34 +0900

    Update General_functions.py

--- General_functions.py
+++ General_functions.py
@@ -185,7 +185,7 @@ def restore_image(batch_image, mean, std):
 
 
 def delete_model(gabage_model):
-    print(f"str({gabage_model}) is removed from GPU!")
+    print("Model is removed from GPU!")
     del gabage_model
     gc.collect() 
     torch.cuda.empty_cache()
