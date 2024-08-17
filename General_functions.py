32a91e618de7aa3c177103678480ff3c9b21a890
parksj003 <40786853+parksj003@users.noreply.github.com>
2024-08-18 06:08:32 +0900

    Update General_functions.py

--- General_functions.py
+++ General_functions.py
@@ -190,3 +190,13 @@ def delete_model(gabage_model):
     gc.collect() 
     torch.cuda.empty_cache()
 
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
